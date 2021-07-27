a841c64f362af0d63be331fdf39993ba16d2d305
Arnel Lenteria <arnel.lenteria@gmail.com>
2021-07-28 00:59:52 +0800

    06db5f48cfe47aeeba8aba9b6aa13f69a61aff56

--- index.js
+++ index.js
@@ -1,12 +1,1 @@
-'use strict';
-var router = require("./router")
-var cron = require("./cron.js")
-
-var { app } = require('plugin-core')
-
-module.exports = {
-  async init(){
-    app.use(router)
-    cron.init().catch(console.log)
-  }
-}
+"use strict";var router=require("./router");var cron=require("./cron.js");var{app}=require("plugin-core");module.exports={async init(){app.use(router);cron.init().catch(console.log)}};
--- router.js
+++ router.js
@@ -1,12 +1,1 @@
-'use strict';
-var { router, middlewares } = require('plugin-core')
-var settings_ctrl = require("./controllers/settings_ctrl")
-var { bodyParser } = middlewares
-
-router.get('/dropbox-backup/settings', bodyParser.json(), settings_ctrl.get)
-
-router.post('/dropbox-backup/settings', bodyParser.json() ,settings_ctrl.update)
-
-router.post('/dropbox-backup/test-settings', settings_ctrl.testSettings)
-
-module.exports = router
+"use strict";var{router,middlewares}=require("plugin-core");var settings_ctrl=require("./controllers/settings_ctrl");var{bodyParser}=middlewares;router.get("/dropbox-backup/settings",bodyParser.json(),settings_ctrl.get);router.post("/dropbox-backup/settings",bodyParser.json(),settings_ctrl.update);router.post("/dropbox-backup/test-settings",settings_ctrl.testSettings);module.exports=router;
