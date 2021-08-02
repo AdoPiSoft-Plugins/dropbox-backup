c0edf45f7ecd9f068c66ab5bfc792b17e36f8adf
Arnel Lenteria <arnel.lenteria@gmail.com>
2021-08-02 18:10:38 +0800

    a5b304db8b3554a91d8c74c03d7eda4960cb4fb7

--- assets/scripts/admin/index.js
+++ assets/scripts/admin/index.js
@@ -1,13 +1,1 @@
-(function () {
-  'use strict';
-  var App = angular.module('Plugins')
-  .config(function($stateProvider) {
-    $stateProvider
-    .state('plugins.dropbox_backup', {
-      templateUrl : "/public/plugins/dropbox-backup/views/admin/index.html",
-      controller: 'DropboxBackupPluginCtrl',
-      url: '/dropbox-backup-plugin',
-      title: 'Dropbox Backup'
-    });
-  });
-})();
+(function(){"use strict";var App=angular.module("Plugins").config(function($stateProvider){$stateProvider.state("plugins.dropbox_backup",{templateUrl:"/public/plugins/dropbox-backup/views/admin/index.html",controller:"DropboxBackupPluginCtrl",url:"/dropbox-backup-plugin",title:"Dropbox Backup"})})})();
